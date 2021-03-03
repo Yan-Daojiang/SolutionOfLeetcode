3463a4897958010db5e067e34f592a5d40ebf2a3
Yan-Daojiang <1373836603@qq.com>
2021-03-03 14:17:55 +0800

    2021-03-03

---
+++ ‪heap/medium/347-前-k-个高频元素.java
@@ -0,0 +1,83 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/*
+ * @lc app=leetcode.cn id=347 lang=java
+ *
+ * [347] 前 K 个高频元素
+ *
+ * https://leetcode-cn.com/problems/top-k-frequent-elements/description/
+ *
+ * algorithms
+ * Medium (61.87%)
+ * Likes:    653
+ * Dislikes: 0
+ * Total Accepted:    136.4K
+ * Total Submissions: 220K
+ * Testcase Example:  '[1,1,1,2,2,3]\n2'
+ *
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+ * 你可以按任意顺序返回答案。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return map.get(o1) - map.get(o2);
+            }
+        });
+
+        for (Integer key : map.keySet()) {
+            minHeap.offer(key);
+            if (minHeap.size() > k)
+                minHeap.poll();
+        }
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = minHeap.remove();
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
