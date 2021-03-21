293dc44813a18d7ec19c7dd2d38d2de0eae0c3c7
Yan-Daojiang <1373836603@qq.com>
2021-03-21 16:30:35 +0800

    2021-03-21

--- ‪array/medium/73-矩阵置零.java
+++ ‪array/medium/73-矩阵置零.java
@@ -55,27 +55,52 @@
 // @lc code=start
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean[] rows = new boolean[matrix.length];
-        boolean[] cols = new boolean[matrix[0].length];
+        boolean row = false;
+        boolean col = false;
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
-                } 
+            if (matrix[i][0] == 0) {
+                row = true;
+                break;
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (rows[i] || cols[j]) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                col = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        return;
+        if (row) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (col) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
     }
 }
 // @lc code=end
