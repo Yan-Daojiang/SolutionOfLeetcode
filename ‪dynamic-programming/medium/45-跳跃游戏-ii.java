88132eddfee595ad54ac3afe96b9277fc4c3f1e7
Yan-Daojiang <1373836603@qq.com>
2021-04-11 17:08:47 +0800

    2021-04-11

---
+++ ‪dynamic-programming/medium/45-跳跃游戏-ii.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=45 lang=java
+ *
+ * [45] 跳跃游戏 II
+ *
+ * https://leetcode-cn.com/problems/jump-game-ii/description/
+ *
+ * algorithms
+ * Medium (39.02%)
+ * Likes:    907
+ * Dislikes: 0
+ * Total Accepted:    117.6K
+ * Total Submissions: 300.8K
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ * 
+ * 示例:
+ * 
+ * 输入: [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 
+ * 
+ * 说明:
+ * 
+ * 假设你总是可以到达数组的最后一个位置。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int jump(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[len - 1];
+    }
+}
+// @lc code=end
+
