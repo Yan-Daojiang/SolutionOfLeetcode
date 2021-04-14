33822462d071c60832c056c2f2d6818e363f6a87
Yan-Daojiang <1373836603@qq.com>
2021-04-14 16:22:40 +0800

    2021-04-14

---
+++ ‪greedy/hard/135-分发糖果.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=135 lang=java
+ *
+ * [135] 分发糖果
+ *
+ * https://leetcode-cn.com/problems/candy/description/
+ *
+ * algorithms
+ * Hard (48.11%)
+ * Likes:    529
+ * Dislikes: 0
+ * Total Accepted:    72.2K
+ * Total Submissions: 150.1K
+ * Testcase Example:  '[1,0,2]'
+ *
+ * 老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+ * 
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ * 
+ * 
+ * 每个孩子至少分配到 1 个糖果。
+ * 评分更高的孩子必须比他两侧的邻位孩子获得更多的糖果。
+ * 
+ * 
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[1,0,2]
+ * 输出：5
+ * 解释：你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[1,2,2]
+ * 输出：4
+ * 解释：你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ * ⁠    第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        
+        if (ratings == null || len == 0)
+            return 0;
+
+        int[] nums = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                nums[i] = nums[i - 1] + 1;
+            } else {
+                nums[i] = 1;
+            }
+        }
+
+        int ans = 0;
+        int right = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (i < len - 1 && ratings[i] > ratings[i + 1]) {
+                ++right;
+            } else {
+                right = 1;
+            }
+
+
+            ans += Math.max(right, nums[i]);
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
---
+++ ‪sort/easy/350-两个数组的交集-ii.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=350 lang=java
+ *
+ * [350] 两个数组的交集 II
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (54.18%)
+ * Likes:    476
+ * Dislikes: 0
+ * Total Accepted:    187.4K
+ * Total Submissions: 345.8K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ * 
+ * 
+ * 
+ * 说明：
+ * 
+ * 
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        
+        if (len1 == 0 || len2 == 0) {
+            return new int[0];
+        }
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> common = new ArrayList<>();
+        
+        int i = 0, j = 0;
+        while (i < len1 && j < len2) {
+            if (nums1[i] == nums2[j]) {
+                common.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] ans = new int[common.size()];
+
+        for (int index = 0; index < common.size(); index++) {
+            ans[index] = common.get(index);
+        }
+        
+        return ans;
+    }
+}
+// @lc code=end
+
