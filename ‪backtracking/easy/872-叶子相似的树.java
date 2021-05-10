f8f74aa5175b83c89592853a0c6b6cf130bb27d8
yandaojiang <1373836603@qq.com>
2021-05-10 22:17:10 +0800

    2021-05-10

---
+++ ‪backtracking/easy/872-叶子相似的树.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=872 lang=java
+ *
+ * [872] 叶子相似的树
+ *
+ * https://leetcode-cn.com/problems/leaf-similar-trees/description/
+ *
+ * algorithms
+ * Easy (62.57%)
+ * Likes:    144
+ * Dislikes: 0
+ * Total Accepted:    51.6K
+ * Total Submissions: 79.1K
+ * Testcase Example:  '[3,5,1,6,2,9,8,null,null,7,4]\n' +
+  '[3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]'
+ *
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ * 
+ * 
+ * 
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ * 
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ * 
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 =
+ * [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root1 = [1], root2 = [1]
+ * 输出：true
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root1 = [1], root2 = [2]
+ * 输出：false
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root1 = [1,2], root2 = [2,2]
+ * 输出：true
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 给定的两棵树可能会有 1 到 200 个结点。
+ * 给定的两棵树上的值介于 0 到 200 之间。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = leafList(root1);
+        List<Integer> list2 = leafList(root2);
+
+        if (list1.size() != list2.size())
+            return false;
+
+
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) != list2.get(i))
+                return false;
+        }
+
+        return true;
+    }
+
+    public List<Integer> leafList(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        visit(root, list);
+
+        return list;
+    }
+
+    public void visit(TreeNode root, List<Integer> leafList) {
+        if (root == null)
+            return;
+        
+        TreeNode node = root;
+        if (root.left == null && root.right == null) 
+            leafList.add(root.val);
+        
+        visit(root.left, leafList);
+        visit(root.right, leafList);
+    }
+}
+// @lc code=end
+
