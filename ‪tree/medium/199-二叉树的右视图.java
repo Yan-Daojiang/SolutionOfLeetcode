dd46bb0b520d51f6de311d0f2a183b0df924cd0b
yandaojiang <1373836603@qq.com>
2021-07-06 14:41:23 +0800

    2021-07-06

---
+++ ‪tree/medium/199-二叉树的右视图.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=199 lang=java
+ *
+ * [199] 二叉树的右视图
+ *
+ * https://leetcode-cn.com/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (64.90%)
+ * Likes:    487
+ * Dislikes: 0
+ * Total Accepted:    117.1K
+ * Total Submissions: 180.4K
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ * 
+ * ⁠  1            <---
+ * ⁠/   \
+ * 2     3         <---
+ * ⁠\     \
+ * ⁠ 5     4       <---
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+                if (i == size - 1) {
+                    ans.add(node.val);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
